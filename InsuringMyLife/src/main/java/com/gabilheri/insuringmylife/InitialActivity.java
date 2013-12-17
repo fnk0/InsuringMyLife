fb62307ff1afd059d0972b6b1fb6355fca52875a
fnk0 <marcusandreog@gmail.com>
2013-12-16 23:16:03 -0600

    Initial commit Added Login and Register Created InitialActivity

--- InsuringMyLife/src/main/java/com/gabilheri/insuringmylife/InitialActivity.java
+++ InsuringMyLife/src/main/java/com/gabilheri/insuringmylife/InitialActivity.java
@@ -1,15 +1,9 @@
 package com.gabilheri.insuringmylife;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 public class InitialActivity extends Activity {
 
@@ -18,11 +12,6 @@ public class InitialActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
 
 
@@ -46,20 +35,5 @@ public class InitialActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_initial, container, false);
-            return rootView;
-        }
-    }
 
 }
