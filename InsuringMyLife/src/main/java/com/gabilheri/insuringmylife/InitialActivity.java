5bc2b89cb43e3bf8ed8e19495c0e5a0d09293ff8
fnk0 <marcusandreog@gmail.com>
2013-12-19 00:52:47 -0600

    Added the Drawer and set up the Logout button on the Drawer.

--- InsuringMyLife/src/main/java/com/gabilheri/insuringmylife/InitialActivity.java
+++ InsuringMyLife/src/main/java/com/gabilheri/insuringmylife/InitialActivity.java
@@ -4,26 +4,39 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class InitialActivity extends Activity {
 
-    private static final int MENU_LOGOUT = 300;
+    private String[] drawerOptions;
+    private DrawerLayout optionsDrawerLayout;
+    private ListView drawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
+        drawerOptions = getResources().getStringArray(R.array.drawer_options);
+        optionsDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the listView
+        drawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, drawerOptions));
+        drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
         // Inflate the menu; this adds items to the action bar if it is present.
-        menu.add(0, MENU_LOGOUT, 0, getString(R.string.logout));
-        return true;
+        return false;
     }
 
     @Override
@@ -31,20 +44,31 @@ public class InitialActivity extends Activity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        switch (id) {
-            case MENU_LOGOUT:
-                SharedPreferences loginPref = getSharedPreferences("loginPref", 0);
-                SharedPreferences.Editor loginEditor = loginPref.edit();
-                loginEditor.clear();
-                loginEditor.commit();
 
-                Intent i = new Intent(InitialActivity.this, LoginActivity.class);
-                startActivity(i);
-                break;
-        }
         return super.onOptionsItemSelected(item);
     }
 
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+
+    private void selectItem(int position) {
+
+        if(drawerOptions[position].equals("Logout")) {
+            SharedPreferences loginPref = getSharedPreferences("loginPref", 0);
+            SharedPreferences.Editor loginEditor = loginPref.edit();
+            loginEditor.clear();
+            loginEditor.commit();
+
+            Intent i = new Intent(InitialActivity.this, LoginActivity.class);
+            startActivity(i);
+        }
+    }
 
 }
